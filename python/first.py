2a3653efdbe69493ee4aa804332d267bc134655f
Aku Sarma <akusarma1702@gmail.com>
2024-09-05 20:39:51 +0530

    added the python class files

---
+++ python/first.py
@@ -0,0 +1,12 @@
+print("I'm on my way to get my offer letter")
+print(325+470)
+# Talent is nothing, consistency is the key
+a = 8
+b = 9
+sum = a + b
+print(sum)
+
+#wap to print even number in the range of 1 to 100
+for i in range(101):
+    if i%2 == 0:
+        print(i, end=" ")
