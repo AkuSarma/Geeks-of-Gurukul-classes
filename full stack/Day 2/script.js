d4e0e96535cbb6e75429ad6e46029a2dd9f8d139
Aku Sarma <akusarma1702@gmail.com>
2024-09-12 14:54:24 +0530

    fullstack day 2

---
+++ full stack/Day 2/script.js
@@ -0,0 +1,32 @@
+let a = 5;
+
+console.log(a);
+
+let x = "20";
+let y = 20;
+console.log(x == y);
+console.log(x === y);
+
+console.log(a++);
+console.log(++a);
+
+let isRaining = false;
+
+if (isRaining) {
+  console.log("lets watch a movie");
+}
+
+a = 20;
+let b = 20;
+
+if (a > b) {
+  console.log("A is greater than B");
+}
+
+if (b > a) {
+  console.log("B is greater than A");
+}
+
+if (a === b) {
+  console.log("Both A and B are equal");
+}
